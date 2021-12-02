70d2dcb426710681b9de7a79f7bd7b2dc5c5499b
Saurav Jha <sauravjha24@gmail.com>
2021-12-02 19:11:36 +1100

    remove unwanted code

--- src/main/kotlin/dynamicprogramming/Palindromepartitioning.kt
+++ src/main/kotlin/dynamicprogramming/Palindromepartitioning.kt
@@ -34,38 +34,3 @@ fun longestPalindrome(value: String): String {
     }
     return palindromeLongest.first
 }
-
-
-class Solution {
-    fun longestPalindrome(value: String): String {
-        val palindromeLongest = LinkedList<String>()
-        (value.indices).map { i ->
-            (i..value.length).map { j ->
-                val subString = value.subSequence(i, j).toString()
-                if (isPalindrome(subString, 0, subString.length - 1)) {
-                    if (palindromeLongest.size == 0) {
-                        palindromeLongest.add(subString)
-                    } else {
-                        if (palindromeLongest.last.length < subString.length) {
-                            palindromeLongest.pop()
-                            palindromeLongest.add(subString)
-                        }
-                    }
-                }
-            }
-        }
-        return palindromeLongest.first
-    }
-
-    fun isPalindrome(value: String, startIndex: Int, endIndex: Int): Boolean {
-        if (endIndex < startIndex) {
-            return false
-        }
-        (0..(endIndex - startIndex) / 2).forEach {
-            if (value[startIndex + it] != value[endIndex - it]) {
-                return false
-            }
-        }
-        return true
-    }
-}
